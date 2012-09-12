5613dd0c55806a818860d60e9a114d69f687ae1e
humberos <humberos@gmail.com>
2012-09-12 19:29:42 -0300

    Added user configurable WiFi sleep policy
    Improve background Wifi performance (PM_MAX or PM_FAST)
    
    Change-Id: I166f5c543a88d2536c3c3b754e979688fdd3d340

--- GNexusParts/src/com/cyanogenmod/settings/device/WifiPowerManagement.java
+++ GNexusParts/src/com/cyanogenmod/settings/device/WifiPowerManagement.java
@@ -17,57 +17,45 @@
 package com.cyanogenmod.settings.device;
 
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.SystemProperties;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceManager;
 
-public class Hspa implements OnPreferenceChangeListener {
+public class WifiPowerManagement implements OnPreferenceChangeListener {
 
-    private static final String APK_FILE = "/system/app/SamsungServiceMode.apk";
-    private static final String HSPA_PROP = "ro.gnexusparts.rild.hspa";
-    private static final String HSPA_PROP_ENABLED = "1";
-
-    private Context mCtx;
-
-    public Hspa(Context context) {
-        mCtx = context;
-    }
+    private static final String FILE = "/sys/module/bcmdhd/parameters/wifi_pm";
 
     public static boolean isSupported() {
-        String mHspa = SystemProperties.get(HSPA_PROP,"0");
-        if (mHspa.equals(HSPA_PROP_ENABLED)) {
-            return true;
-        } else {
-            return false;
-        }
+        return Utils.fileExists(FILE);
     }
 
-    /**
-     * Restore HSPA setting from SharedPreferences. (Write to kernel.)
-     * @param context       The context to read the SharedPreferences from
-     */
     public static void restore(Context context) {
         if (!isSupported()) {
             return;
         }
 
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
-        sendIntent(context, sharedPrefs.getString(DeviceSettings.KEY_HSPA, "23"));
+        Utils.writeValue(FILE, sharedPrefs.getString(DeviceSettings.KEY_WIFI_PM, "0"));
     }
 
-    @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        sendIntent(mCtx, (String) newValue);
+        Utils.writeValue(FILE, (String) newValue);
+        updateSummary((ListPreference) preference, Integer.parseInt(newValue.toString()));
         return true;
     }
 
-    private static void sendIntent(Context context, String value) {
-        Intent i = new Intent("com.cyanogenmod.SamsungServiceMode.EXECUTE");
-        i.putExtra("sub_type", 20); // HSPA Setting
-        i.putExtra("data", value);
-        context.sendBroadcast(i);
+    public static void updateSummary(ListPreference preference, int value) {
+        final CharSequence[] entries = preference.getEntries();
+        final CharSequence[] values = preference.getEntryValues();
+        int best = 0;
+        for (int i = 0; i < values.length; i++) {
+            int summaryValue = Integer.parseInt(values[i].toString());
+            if (value >= summaryValue) {
+                best = i;
+            }
+        }
+        preference.setSummary(entries[best].toString());
     }
 }
