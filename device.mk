da98a5abd0d0643710cc4dd90e7daca24929734a
humberos <humberos@gmail.com>
2012-09-23 00:47:40 -0300

    Add zRam support
    Dependencies: http://review.cyanogenmod.com/#/c/23775/
    
    Change-Id: I55c56e58487c35a05fa81c2c5a6a5fc6f9ac67d3

--- device.mk
+++ device.mk
@@ -41,7 +41,8 @@ PRODUCT_PACKAGES += \
 	nfc.tuna
 
 PRODUCT_PACKAGES += \
-	power.tuna
+	power.tuna \
+	GNexusParts
 
 # Audio
 PRODUCT_PACKAGES += \
@@ -64,6 +65,7 @@ PRODUCT_COPY_FILES += \
 	device/samsung/tuna/init.tuna.usb.rc:root/init.tuna.usb.rc \
 	device/samsung/tuna/fstab.tuna:root/fstab.tuna \
 	device/samsung/tuna/ueventd.tuna.rc:root/ueventd.tuna.rc \
+	device/samsung/tuna/init.zram:root/sbin/init.zram \
 	device/samsung/tuna/media_profiles.xml:system/etc/media_profiles.xml \
 	device/samsung/tuna/media_codecs.xml:system/etc/media_codecs.xml \
 	device/samsung/tuna/gps.conf:system/etc/gps.conf
