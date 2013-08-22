6f2e7473fa29cb143ae4037b9e2262a56590ea27
Danesh M <daneshm90@gmail.com>
2013-08-22 12:26:33 +0200

    tuna: Cleanup hardware tunables
    Change-Id: I2aa1d746ec6c8372f80e022b786078c5ded68612

--- cmhw/org/cyanogenmod/hardware/VibratorHW.java
+++ cmhw/org/cyanogenmod/hardware/VibratorHW.java
@@ -16,8 +16,8 @@
 
 package org.cyanogenmod.hardware;
 
-import org.cyanogenmod.hardware.util.FileUtils;
 import java.io.File;
+import org.cyanogenmod.hardware.util.FileUtils;
 
 public class VibratorHW {
 
@@ -30,18 +30,23 @@ public class VibratorHW {
     public static int getMaxIntensity()  {
         return 100;
     }
+
     public static int getMinIntensity()  {
         return 0;
     }
+
     public static int getWarningThreshold()  {
         return -1;
     }
+
     public static int getCurIntensity()  {
         return Integer.parseInt(FileUtils.readOneLine(AMP_PATH));
     }
+
     public static int getDefaultIntensity()  {
         return 100;
     }
+
     public static boolean setIntensity(int intensity)  {
         return FileUtils.writeLine(AMP_PATH, String.valueOf(intensity));
     }
