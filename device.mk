bc15eb9e9f638106e2d59a1c15ef68a0b2a0c117
Lorenzo M <djmesias@yahoo.es>
2013-12-24 01:11:37 +0100

    tuna: Dexopt /system apps to /cache
     After http://review.cyanogenmod.org/56061
    all dexopt system apps goes to /data. If maguro device
    has 512MB in /cache, why doesn't use it?.
    
     With this commit, all dex from system apps goes to /cache
    Tested with ROM+Full GApps and we have over 250 MB free
    in /cache. Furthermore we gain a precious internal data space.
    
    Change-Id: Id8c0f794b347229e3866b86fcfeee909ca7bf0c5

--- device.mk
+++ device.mk
@@ -200,6 +200,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PROPERTY_OVERRIDES += \
         ro.zygote.disable_gl_preload=true
 
+# Dexopt /system apps to /cache
+dalvik.vm.dexopt-data-only=0
+
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 
 $(call inherit-product-if-exists, vendor/nxp/pn544/nxp-pn544-fw-vendor.mk)
