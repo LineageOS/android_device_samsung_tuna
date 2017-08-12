664a8bd7828fce0b4fbed0d9cf72ddb4c78f23bc
nailyk-fr <nailyk_git@nailyk.fr>
2017-08-12 17:19:44 +0200

    tuna: twrp: Add encryption necessary dependencies
    Change-Id: Id5b80699f95d2aa50aabd9289dd8de9cea3037b6

--- twrp.mk
+++ twrp.mk
@@ -14,3 +14,5 @@ TW_EXTERNAL_STORAGE_MOUNT_POINT := "usb-otg"
 TW_MAX_BRIGHTNESS := 255
 TW_BRIGHTNESS_PATH := /sys/class/backlight/s6e8aa0/brightness
 TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/platform/omap/omap_temp_sensor.0/temperature
+TARGET_RECOVERY_DEVICE_MODULES := libtf_crypto_sst smc_pa_ctrl tf_daemon keystore.tuna
+
